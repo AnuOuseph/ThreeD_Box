51d81ad6d5905e2109b9df14bbff69a5a5c5fb9c
AnuOuseph <anuouseph04@gmail.com>
2023-10-28 18:23:22 +0530

    Colors added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -38,6 +38,40 @@ function CubeWithPictures() {
   );
 }
 
+function CubeWithColors() {
+  const colors = [
+    'blue',
+    'red',
+    'yellow',
+    'green',
+    'orange',
+    'black',
+  ];
+
+  const materials = colors.map((color) => {
+    const material = new THREE.MeshBasicMaterial({ color: color });
+    return material;
+  });
+
+  const cubeRef = useRef();
+
+  useFrame(() => {
+    if (cubeRef.current) {
+      cubeRef.current.rotation.x += 0.01;
+      cubeRef.current.rotation.y += 0.01;
+    }
+  });
+
+  return (
+    <mesh ref={cubeRef}>
+      <boxGeometry args={[2, 2, 2]} />
+      {materials.map((material, index) => (
+        <meshLambertMaterial key={index} attach={`material-${index}`} color={material.color} />
+      ))}
+    </mesh>
+  );
+}
+
 function App() {
   return (
     <div style={{ width: '100vw', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
@@ -45,6 +79,11 @@ function App() {
         <ambientLight />
         <pointLight position={[10, 10, 10]} />
         <CubeWithPictures />
+      </Canvas>
+      <Canvas camera={{ position: [0, 0, 10] }}>
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <CubeWithColors />
       </Canvas>
     </div>
   );
